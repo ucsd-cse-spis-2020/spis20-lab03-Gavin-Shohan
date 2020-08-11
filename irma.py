355c7b0e0dba1a59823e6ea58d22536d3736ac3c
Shohan Ramesh <shohanaadithya@gmail.com>
2020-08-11 00:00:25 +0000

    added category check function to irma.py and start on hurricane tracing

--- irma.py
+++ irma.py
@@ -4,9 +4,7 @@ import csv
 def irma_setup():
     """Creates the Turtle and the Screen with the map background
        and coordinate system set to match latitude and longitude.
-
        :return: a tuple containing the Turtle and the Screen
-
        DO NOT CHANGE THE CODE IN THIS FUNCTION!
     """
     import tkinter
@@ -34,21 +32,7 @@ def irma_setup():
 
     return (t, wn, map_bg_img)
 
-def determineCategory(speed):
-    """Determines the hurricane category according to its speed"""
-    speed = int(speed)
-    if (speed >= 74 and speed <= 95):
-        return 1
-    elif (speed > 95 and speed <= 110):
-        return 2
-    elif (speed > 110 and speed <= 129):
-        return 3
-    elif (speed > 129 and speed <= 156):
-        return 4
-    elif (speed > 156):
-        return 5
-    else:
-        return 0
+        
 
 def irma():
     """Animates the path of hurricane Irma
@@ -56,7 +40,13 @@ def irma():
     # Do not change this line
     # t is the turtle, and you will not need the other variables
     (t, wn, map_bg_img) = irma_setup()
-
+    t.hideturtle()
+    t.penup()
+    t.setpos(-30.3, 16.4)
+    t.showturtle()
+    t.pendown()
+    
+    
     hurricaneFile = "data/irma.csv"
     # The line below is a little magical. It opens the file,
     # with awareness of any errors that might occur.
@@ -72,16 +62,6 @@ def irma():
         # look into Python's built-in next function
         #(https://docs.python.org/3/library/functions.html#next)
         # pointreader is an iterator
-
-        # Skipping first line
-        next(pointreader)
-        t.pencolor("red")
-
-        # Setting initial position
-        current = next(pointreader)
-        t.up()
-        t.goto(float(current[3]), float(current[2]))
-        t.down()
         next(pointreader)
 
         for row in pointreader:
@@ -91,43 +71,34 @@ def irma():
             # row in the file.
             # Make sure you understand what is happening here.
             # Then, you'll need to change this code
+            
+            t.setpos(float(row[3]),float(row[2]))  
+             
+            
+    
+
             print("Date:", row[0], "Time:", row[1])
 
-            # Settings for turtle
-            category = determineCategory(row[4])
-            if (category == 0):
-                t.pencolor("white")
-                t.width(1)
-            elif (category == 1):
-                t.pencolor("blue")
-                t.width(3)
-                t.write("1", False, align="center")
-            elif (category == 2):
-                t.pencolor("green")
-                t.width(5)
-                t.write("2", False, align="center")
-            elif (category == 3):
-                t.pencolor("yellow")
-                t.width(7)
-                t.write("3", False, align="center")
-            elif (category == 4):
-                t.pencolor("orange")
-                t.width(8)
-                t.write("4", False, align="center")
-            elif (category == 5):
-                t.pencolor("red")
-                t.width(10)
-                t.write("5", False, align="center")
 
-            # Latitude = Y, Longitude = X
-            t.goto(float(row[3]), float(row[2]))
-
-    # Added code
-    wn.exitonclick()
 
     # Hack to make sure a reference to the background image stays around
     # Do not remove or change this line
     return map_bg_img
+
+def categoryCheck(windSpeed):
+    if(windSpeed < 33):
+        return 0
+    elif (33 <= windSpeed < 43):
+        return 1
+    elif (43 <= windSpeed < 50):
+        return 2
+    elif (50 <= windSpeed < 58):
+        return 3
+    elif (58 <= windSpeed < 70):
+        return 4
+    elif (windSpeed > 70):
+        return 5
+        
 
 
 # Feel free to add "helper" functions here
@@ -135,3 +106,4 @@ def irma():
 
 if __name__ == "__main__":
     bg=irma()
+
